9c2f72ebc0870cb8f8238ffc10176f0b7cd73ed6
Leonardo Franco | Montdata Tec LTDA <66044013+leonardofranco-montdata@users.noreply.github.com>
2021-04-29 19:27:05 -0300

    Update install_montd_agent.sh

--- install_montd_agent.sh
+++ install_montd_agent.sh
@@ -1,4 +1,6 @@
-groupadd node_exporter
+#!/bin/sh
+# type 0=Microservice, 1=Service
+type=$1
 useradd --no-create-home --shell /bin/false node_exporter
 curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest | grep browser_download_url | grep linux-amd64 | cut -d '"' -f 4 | wget -qi -
 tar zxvf node_exporter-*
@@ -6,10 +8,10 @@ cp node_exporter-*/node_exporter /usr/local/bin
 chown node_exporter:node_exporter /usr/local/bin/node_exporter
 rm -rf node_exporter-*
 
-
-
-
-cat <<EOF > /etc/systemd/system/node_exporter.service
+if [ ${type} == 1 ]
+then
+echo "entrou no if"
+cat <<EOF >  /etc/systemd/system/node_exporter.service
 [Unit]
 Description=Node Exporter
 Wants=network-online.target
@@ -22,10 +24,7 @@ ExecStart=/usr/local/bin/node_exporter
 [Install]
 WantedBy=multi-user.target
 EOF
-
-
-
-
 systemctl daemon-reload
 systemctl start node_exporter
 systemctl enable node_exporter
+fi
